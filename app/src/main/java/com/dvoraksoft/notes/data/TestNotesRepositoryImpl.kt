9fe6516cc748613500d76653a90230313a6564d9
Pavel Potapov <potapov@goozix.com>
2026-05-10 13:49:16 +0300

    Add test repository

---
+++ app/src/main/java/com/dvoraksoft/notes/data/TestNotesRepositoryImpl.kt
@@ -0,0 +1,81 @@
+package com.dvoraksoft.notes.data
+
+import com.dvoraksoft.notes.domain.Note
+import com.dvoraksoft.notes.domain.NotesRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
+
+class TestNotesRepositoryImpl : NotesRepository {
+
+    private val notesListFlow = MutableStateFlow<List<Note>>(listOf())
+
+    override fun addNote(note: Note) {
+// Variant 1
+//        val newNotes = notesListFlow.value.toMutableList()
+//        newNotes.add(note)
+//        notesListFlow.value = newNotes
+
+// Variant 2
+//        notesListFlow.update {
+//            it.toMutableList().apply {
+//                add(note)
+//            }
+//        }
+
+// Variant 3
+        notesListFlow.update {
+            it + note
+        }
+    }
+
+    override fun deleteNote(noteId: Int) {
+        notesListFlow.update { oldList ->
+            oldList.toMutableList().apply {
+                removeIf { it.id == noteId }
+            }
+        }
+    }
+
+    override fun editNote(note: Note) {
+        notesListFlow.update { oldList ->
+            oldList.map {
+                if (it.id == note.id) {
+                    note
+                } else {
+                    it
+                }
+            }
+        }
+    }
+
+    override fun getAllNotes(): Flow<List<Note>> {
+        return notesListFlow.asStateFlow()
+    }
+
+    override fun getNote(noteId: Int): Note {
+        return notesListFlow.value.first { it.id == noteId }
+    }
+
+    override fun searchNotes(query: String): Flow<List<Note>> {
+        return notesListFlow.map { currentList ->
+            currentList.filter {
+                it.title.contains(query) || it.content.contains(query)
+            }
+        }
+    }
+
+    override fun switchPinnedStatus(noteId: Int) {
+        notesListFlow.update { oldList ->
+            oldList.map {
+                if (it.id == noteId) {
+                    it.copy(isPinned = it.isPinned)
+                } else {
+                    it
+                }
+            }
+        }
+    }
+}
