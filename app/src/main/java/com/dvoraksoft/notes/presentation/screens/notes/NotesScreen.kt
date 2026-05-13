9534cdbaffa65f14e061a0c1f83b4fd86617b374
Pavel Potapov <potapov@goozix.com>
2026-05-13 16:06:55 +0300

    Add DateFormatter

--- app/src/main/java/com/dvoraksoft/notes/presentation/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/dvoraksoft/notes/presentation/screens/notes/NotesScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -28,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,6 +38,7 @@ import com.dvoraksoft.notes.domain.Note
 import com.dvoraksoft.notes.presentation.ui.theme.NotesTheme
 import com.dvoraksoft.notes.presentation.ui.theme.OtherNotesColors
 import com.dvoraksoft.notes.presentation.ui.theme.PinnedNotesColors
+import com.dvoraksoft.notes.presentation.utils.DateFormatter
 
 @Preview
 @Composable
@@ -99,6 +102,7 @@ fun NotesScreen(
                     key = { _, note -> note.id }
                 ) { index, note ->
                     NoteCard(
+                        modifier = Modifier.widthIn(max = 160.dp),
                         note = note,
                         onNoteClick = {
                             viewModel.processCommand(NotesCommand.EditNote(note))
@@ -248,11 +252,13 @@ fun NoteCard(
         Text(
             text = note.title,
             fontSize = 14.sp,
-            color = MaterialTheme.colorScheme.onSurface
+            maxLines = 1,
+            color = MaterialTheme.colorScheme.onSurface,
+            overflow = TextOverflow.Ellipsis
         )
         Spacer(modifier = Modifier.height(8.dp))
         Text(
-            text = note.updatedAt.toString(),
+            text = DateFormatter.formatDateToString(note.updatedAt),
             fontSize = 12.sp,
             color = MaterialTheme.colorScheme.onSurfaceVariant
         )
@@ -260,8 +266,10 @@ fun NoteCard(
         Text(
             text = note.content,
             fontSize = 12.sp,
+            maxLines = 3,
             color = MaterialTheme.colorScheme.onSurface,
-            fontWeight = FontWeight.Medium
+            fontWeight = FontWeight.Medium,
+            overflow = TextOverflow.Ellipsis
         )
     }
 }
