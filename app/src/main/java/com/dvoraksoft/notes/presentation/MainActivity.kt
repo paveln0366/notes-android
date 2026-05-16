bf9832c689c71667ee0ce52240e235d9b53b47cc
Pavel Potapov <potapov@goozix.com>
2026-05-16 16:59:50 +0300

    Add EditNote

--- app/src/main/java/com/dvoraksoft/notes/presentation/MainActivity.kt
+++ app/src/main/java/com/dvoraksoft/notes/presentation/MainActivity.kt
@@ -5,7 +5,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.dvoraksoft.notes.presentation.screens.creation.CreateNoteScreen
+import com.dvoraksoft.notes.presentation.screens.editing.EditNoteScreen
 import com.dvoraksoft.notes.presentation.ui.theme.NotesTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,11 +14,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NotesTheme {
-                CreateNoteScreen(
+                EditNoteScreen(
+                    noteId = 5,
                     onFinished = {
                         Log.d("CreateNoteScreen", "Finished")
                     }
                 )
+//                CreateNoteScreen(
+//                    onFinished = {
+//                        Log.d("CreateNoteScreen", "Finished")
+//                    }
+//                )
 //                NotesScreen(
 //                    onNoteClick = {
 //                        Log.d("MainActivity", "onNoteClick: $it")
