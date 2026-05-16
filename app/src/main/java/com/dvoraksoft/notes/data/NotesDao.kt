4af0ce251a24e8f54cf3aac65bed224b0b43b9f4
Pavel Potapov <potapov@goozix.com>
2026-05-16 22:50:43 +0300

    Add NotesRepositoryImpl

--- app/src/main/java/com/dvoraksoft/notes/data/NotesDao.kt
+++ app/src/main/java/com/dvoraksoft/notes/data/NotesDao.kt
@@ -12,6 +12,9 @@ interface NotesDao {
     @Query("SELECT * FROM notes ORDER BY updatedAt DESC")
     fun getAllNotes(): Flow<List<NoteDbModel>>
 
+    @Query("SELECT * FROM notes WHERE id == :noteId")
+    fun getNote(noteId: Int): NoteDbModel
+
     @Query("SELECT * FROM notes WHERE title LIKE '%' || :query || '%' OR content LIKE '%' || :query || '%' ORDER BY updatedAt DESC")
     fun searchNotes(query: String): Flow<List<NoteDbModel>>
 
